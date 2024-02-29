f03512d41cc79af480b543318def0d3c30da938f
Tiobee <bernardo.budal10@gmail.com>
2024-02-29 16:27:13 -0300

    Inicial

---
+++ config/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+// Vá até o final da palavra contendo o componente
+// que deseja importar e aperte CRTL + Espaço
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#9d6ff7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo1: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  titulo2: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,34 @@
+import { ScrollView, Text, View } from "react-native-web";
+import styles from "../config/style";
+
+// export default function já declara a exportação do 
+// componente para ser usado posteriormente
+export default function HomeScreen(){
+    
+      // Estrutura básica de um componente
+    // precisa ter um retorno
+    return (
+        <View style={styles.container}>
+      <ScrollView>
+      <Text style={styles.titulo1}>Q!Cellbit</Text>
+        <Text style={styles.titulo2}>A personalidade de Cellbit é algo muito estranho de se escrever explicitamente, já que está sempre mudando e tendo novidades reveladas sobre ela. Muitas vezes ele é gentil com pessoas que não conhece bem e com aqueles que são considerados amigos por ele, mas quando quer odiar alguém, ele odeia até o último minuto. O passado de Cellbit também influenciou muito em sua personalidade agora, coisas que o fizeram se tornar uma pessoa mais fria e perigosa, ele matou milhares de pessoas e gostou disso.
+          Cellbit, em seus momentos mais íntimos, muitas vezes pensa no que fez para merecer ser tão maltratado e ter que se tornar quem é agora, já que, segundo ele, era uma pessoa amorosa e gentil antes de todos os acontecimentos que o traumatizaram fortemente.
+          Frequentemente ardiloso e astuto, Cellbit tende a ocultar informações e está sujeito a se meter em encrencas, se isso for o necessário para proteger com sua família e amigos. Apesar da sua personalidade protetora, ele também tem instintos assassinos, conforme implícito por ele mesmo e pela dupla TazerCraft.
+          Investigando profundamente a natureza misteriosa da Ilha Quesadilla, Cellbit provou ser um detetive e solucionador de enigmas muito competente. Demonstrando ser uma pessoa muito racional e competente, reformulou e tornou-se líder da Ordem, onde investiga e articula planos contra a Federação. No entanto, depois de ser perseguido e torturado por Cucurucho, devido às suas tentativas de descobrir mais sobre a Federação, ele se tornou severamente paranoico e parece muito mais instável, exibindo suas tendências violentas com mais frequência contra aqueles que ele considera seus inimigos.
+          Embora Cellbit possa parecer frio às vezes, ele também demonstra um lado mais compassivo e empático. Ele mostra preocupação genuína por seus colegas e está disposto a ajudá-los quando necessário. Ele é bastante generoso e pode ser descrito como altruísta, colocando o bem-estar daqueles que ele se importa acima até mesmo de si mesmo. Ele tem uma natureza sacrificial e não se importa em se colocar em situações perigosas para alcançar um objetivo maior em prol do bem-estar de todos na ilha. Isso pode ser demonstrado quando ele admitiu que não tinha medo de morrer após se juntar à Federação se seu sacrifício significasse que todos poderiam escapar da ilha no final. Embora esse lado de Cellbit possa ser visto como um testemunho de sua motivação e vontade de proteger a todos, também pode ser visto como uma fachada para esconder suas próprias cicatrizes emocionais e traumas passados.
+          <br></br><br></br><br></br>
+          .sodassap samuart e sianoicome sezirtacic sairpórp saus rednocse arap adahcaf amu omoc otsiv res edop mébmat ,sodot a regetorp ed edatnov e oãçavitom aus ed ohnumetset mu omoc otsiv res assop tiblleC ed odal esse arobmE .lanif on ahli ad rapacse mairedop sodot euq essacifingis oicífircas ues es oãçaredeF à ratnuj es sópa rerrom ed odem ahnit oãn euq uitimda ele odnauq odartsnomed res edop ossI .ahli an sodot ed ratse-meb od lorp me roiam ovitejbo mu raçnacla arap sasogirep seõçautis me racoloc es me atropmi es oãn e laicifircas azerutan amu met elE .omsem is ed omsem éta amica atropmi es ele euq seleuqad ratse-meb o odnacoloc ,atsíurtla omoc otircsed res edop e osoreneg etnatsab é elE .oirássecen odnauq sol-áduja a otsopsid átse e sageloc sues rop aníuneg oãçapucoerp artsom elE .ocitápme e ovissapmoc siam odal mu artsnomed mébmat ele ,sezev sà oirf recerap assop tiblleC arobmE .sogimini sues aredisnoc ele euq seleuqa artnoc aicnêuqerf siam moc satneloiv saicnêdnet saus odnibixe ,levátsni siam otium ecerap e ocionarap etnemareves uonrot es ele ,oãçaredeF a erbos siam rirbocsed ed savitatnet saus sà odived ,ohcurucuC rop odarutrot e odiugesrep res ed sioped ,otnatne oN .oãçaredeF a artnoc sonalp alucitra e agitsevni edno ,medrO ad redíl es-uonrot e uolumrofer ,etnetepmoc e lanoicar otium aossep amu res odnartsnomeD .etnetepmoc otium samgine ed rodanoiculos e eviteted mu res uovorp tiblleC ,allidaseuQ ahlI ad asoiretsim azerutan a etnemadnuforp odnagitsevnI .tfarCrezaT alpud alep e omsem ele rop oticílpmi emrofnoc ,sonissassa sotnitsni met mébmat ele ,arotetorp edadilanosrep aus ad rasepA .sogima e ailímaf aus moc regetorp arap oirássecen o rof ossi es ,sacnercne me retem es a otiejus átse e seõçamrofni ratluco a ednet tiblleC ,otutsa e osolidra etnemetneuqerF .etnemetrof marazitamuart o euq sotnemicetnoca so sodot ed setna litneg e asoroma aossep amu are ,ele odnuges ,euq áj ,aroga é meuq ranrot es euq ret e odatartlam oãt res recerem arap zef euq on asnep sezev satium ,somitní siam sotnemom sues me ,tiblleC .ossid uotsog e saossep ed serahlim uotam ele ,asogirep e airf siam aossep amu ranrot es marezif o euq sasioc ,aroga edadilanosrep aus me otium uoicneulfni mébmat tiblleC ed odassap O .otunim omitlú o éta aiedo ele ,méugla raido reuq odnauq sam ,ele rop sogima sodaredisnoc oãs euq seleuqa moc e meb ecehnoc oãn euq saossep moc litneg é ele sezev satiuM .ale erbos sadalever sedadivon odnet e odnadum erpmes átse euq áj ,etnematicilpxe revercse es ed ohnartse otium ogla é tiblleC ed edadilanosrep A
+          </Text>
+
+      </ScrollView>
+    </View>
+    )
+
+}
+/*
+ Eu não preciso da linha abaixo
+ pois eu declarei a função direta como
+ sendo uma função de export default
+ 
+ export default HomeScreen
+*/
